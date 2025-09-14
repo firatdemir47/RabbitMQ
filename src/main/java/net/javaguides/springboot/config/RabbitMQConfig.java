0de3d3e91bf4028f944337467dd4ecdb9b6aae3f
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-09-14 22:36:05 +0300

    Add JSON routing key, binding and Jackson2JsonMessageConverter to RabbitMQConfig for JSON message support

--- src/main/java/net/javaguides/springboot/config/RabbitMQConfig.java
+++ src/main/java/net/javaguides/springboot/config/RabbitMQConfig.java
@@ -1,9 +1,14 @@
 package net.javaguides.springboot.config;
 
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,15 +19,26 @@ public class RabbitMQConfig {
 	@Value("${rabbitmq.queue.name}")
 	private String queue;
 
+	@Value("${rabbitmq.queue.json.name}")
+	private String jsonQueue;
+
 	@Value("${rabbitmq.exchange.name}")
 	private String exchange;
 
 	@Value("${rabbitmq.routing.key}")
 	private String routingKey;
 
+	@Value("${rabbitmq.routing.json.key}")
+	private String routingJsonKey;
+
 	@Bean
 	public Queue queue() {
 		return new Queue(queue);
+	}
+
+	@Bean
+	public Queue jsonQueue() {
+		return new Queue(jsonQueue);
 	}
 
 	@Bean
@@ -33,5 +49,22 @@ public class RabbitMQConfig {
 	@Bean
 	public Binding binding() {
 		return BindingBuilder.bind(queue()).to(exchange()).with(routingKey);
+	}
+
+	@Bean
+	public Binding jsonbinding() {
+		return BindingBuilder.bind(jsonQueue()).to(exchange()).with(routingJsonKey);
+	}
+
+	@Bean
+	public MessageConverter converter() {
+		return new Jackson2JsonMessageConverter();
+	}
+
+	@Bean
+	public AmqpTemplate ampqTemplate(ConnectionFactory connectionFactory) {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(converter());
+		return rabbitTemplate;
 	}
 }
